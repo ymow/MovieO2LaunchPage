c564b333003ae1122b1467cf57ade91ed14c88b9
ymow <kenshin0812@hotmail.com>
2014-09-22 01:29:52 +0800

    Policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+
  root:to => 'static_pages#home' 
 
   get 'static_pages/download'
-
+  get 'static_pages/Policy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
